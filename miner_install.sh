48735190847fa4c3fb0f511d1236b1740c0e8616
PiotrSobiecki <55161384+PiotrSobiecki@users.noreply.github.com>
2023-11-19 15:03:04 +0100

    Update miner_install.sh

--- miner_install.sh
+++ miner_install.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-nano miner_install.sh
-chmod +x miner_install.sh
-./miner_install.sh
+
+if ! command -v nvm &> /dev/null; then
+    echo "Instalowanie nvm..."
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+    source ~/.bashrc
+fi
+
+set -e
+
 sudo apt-get update && sudo apt-get upgrade -y
+
 sudo apt install -y software-properties-common
 sudo add-apt-repository universe
 sudo apt update
 
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-source ~/.bashrc
 nvm install v16
-
 
 sudo apt-get install git
 
@@ -22,11 +25,9 @@ git submodule update --init --recursive
 chmod +x ./scripts/*
 sudo ./scripts/install_npm_env.sh
 
-
 sudo npm install -g pm2
 
 sudo dpkg --configure -a
-
 sudo apt install -y python3-pip
 
 pip install -e ./
@@ -38,3 +39,4 @@ pip3 uninstall huggingface-hub
 pip3 install 'huggingface-hub==0.18.0'
 pip install openai==0.28
 
+echo "Zakończono instalację pomyślnie."
