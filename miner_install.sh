0b598b9e67c5454fba38093a51e61e4b8e7b0396
PiotrSobiecki <55161384+PiotrSobiecki@users.noreply.github.com>
2023-11-19 15:32:32 +0100

    Update miner_install.sh

--- miner_install.sh
+++ miner_install.sh
@@ -9,7 +9,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 source ~/.bashrc
 nvm install v16
 
-sudo apt-get install git
+sudo apt-get install -y git
 
 git clone https://github.com/commune-ai/commune.git
 cd commune
@@ -23,19 +23,14 @@ sudo npm install -g pm2
 sudo dpkg --configure -a
 sudo apt install -y python3-pip
 
-pip install -e ./ -y
-pip install protobuf==3.20 -y
-pip install streamlit grpcio-tools -y
-pip3 uninstall protobuf -y
-pip3 install 'protobuf>=4.21.6,<5.0dev' -y
-pip3 uninstall huggingface-hub -y
-pip3 install 'huggingface-hub==0.18.0' -y
-pip install openai==0.28 -y
+
+yes | pip install -e ./
+yes | pip install protobuf==3.20
+yes | pip install streamlit grpcio-tools
+yes | pip3 uninstall protobuf
+yes | pip3 install 'protobuf>=4.21.6,<5.0dev'
+yes | pip3 uninstall huggingface-hub
+yes | pip3 install 'huggingface-hub==0.18.0'
+yes | pip install openai==0.28
 
 echo "Można kopać SIR!!!."
-
-
-
-
-
-
