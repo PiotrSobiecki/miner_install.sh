2d367e438d99b6345a34ab7df35851c3426ba754
PiotrSobiecki <55161384+PiotrSobiecki@users.noreply.github.com>
2023-11-19 15:28:45 +0100

    Update miner_install.sh

--- miner_install.sh
+++ miner_install.sh
@@ -5,11 +5,9 @@ sudo apt install -y software-properties-common
 sudo add-apt-repository universe
 sudo apt update
 
-
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 source ~/.bashrc
 nvm install v16
-
 
 sudo apt-get install git
 
@@ -20,21 +18,22 @@ git submodule update --init --recursive
 chmod +x ./scripts/*
 sudo ./scripts/install_npm_env.sh
 
-
 sudo npm install -g pm2
 
 sudo dpkg --configure -a
-
 sudo apt install -y python3-pip
 
-pip install -e ./
-pip install protobuf==3.20
-pip install streamlit grpcio-tools
-pip3 uninstall protobuf
-pip3 install 'protobuf>=4.21.6,<5.0dev' --yes
-pip3 uninstall huggingface-hub --yes
-pip3 install 'huggingface-hub==0.18.0' --yes
-pip install openai==0.28 --yes
+# Install Python packages with -y option to automatically answer yes
+pip install -e ./ -y
+pip install protobuf==3.20 -y
+pip install streamlit grpcio-tools -y
+pip3 uninstall protobuf -y
+pip3 install 'protobuf>=4.21.6,<5.0dev' -y
+pip3 uninstall huggingface-hub -y
+pip3 install 'huggingface-hub==0.18.0' -y
+pip install openai==0.28 -y
+
+echo "Można kopać SIR!!!."
 
 
 
